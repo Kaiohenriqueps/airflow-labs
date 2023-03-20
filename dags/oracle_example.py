fe3ca70c69b496d2937e7ea8c5fbda941ebc1063
Kaiohenriqueps <kh.silva@hotmail.com>
2023-03-20 18:17:22 -0300

    criando uma imagem docker para um oracle db

---
+++ dags/oracle_example.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from airflow.decorators import dag, task
+
+
+@dag(
+    "oracle_example",
+    start_date=datetime(2021, 12, 1),
+    schedule="@daily",
+    catchup=False,
+)
+def my_dag():
+    @task(task_id="connect_to_db")
+    def task_0():
+        from airflow.providers.oracle.hooks.oracle import OracleHook
+
+        hook = OracleHook(oracle_conn_id="oracle_conn_id")
+
+    task_0()
+
+
+my_dag()
